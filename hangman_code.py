7e39fe384d3b7df5dacd48c1ac386834e2c61c6e
AyeshaNaaz17 <ayeshanaazn17@gmail.com>
2022-11-14 20:39:58 +0530

    Committing hangman game

---
+++ hangman_code.py
@@ -0,0 +1,46 @@
+import random
+import hangman_stages
+import hangman_words
+
+word_list = hangman_words.word_list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(hangman_stages.logo)
+
+# print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for i in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"You have already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+
+        print("You have guessed the wrong letter.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print(hangman_stages.stages[lives])
